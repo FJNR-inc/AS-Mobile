79fe1705cd04683d0ef3dac9731146bf5a91e630
Jerome Celle <jeffyer38130@gmail.com>
2020-02-26 16:40:46 +0100

    Update local data from api

--- src/app/datas/artwork_data_type.ts
+++ src/app/datas/artwork_data_type.ts
@@ -5,30 +5,35 @@ export const artworkTypes: IResponseApi<IArtworkType> = {
     count: 10,
     next: null,
     previous: null,
-    results: [{id: 76, name: "Photographie", name_fr: "Photographie", name_en: ""}, {
-        id: 77,
+    results: [{id: 86, name: "Photographie", name_fr: "Photographie", name_en: "Photography"}, {
+        id: 87,
         name: "Performance",
         name_fr: "Performance",
-        name_en: ""
-    }, {id: 78, name: "Installation", name_fr: "Installation", name_en: ""}, {
-        id: 79,
+        name_en: "Performance"
+    }, {id: 88, name: "Installation", name_fr: "Installation", name_en: "Installation"}, {
+        id: 89,
         name: "Sculpture",
         name_fr: "Sculpture",
-        name_en: ""
-    }, {id: 80, name: "Projection Vidéo", name_fr: "Projection Vidéo", name_en: ""}, {
-        id: 81,
+        name_en: "Sculpture"
+    }, {id: 90, name: "Projection Vidéo", name_fr: "Projection Vidéo", name_en: "Video projection"}, {
+        id: 91,
         name: "Texte",
         name_fr: "Texte",
-        name_en: ""
-    }, {id: 82, name: "Installation Sonore", name_fr: "Installation Sonore", name_en: ""}, {
-        id: 83,
+        name_en: "Text"
+    }, {
+        id: 92,
+        name: "Installation Sonore",
+        name_fr: "Installation Sonore",
+        name_en: "Sound Installation"
+    }, {
+        id: 93,
         name: "Oeuvre Vidéo Générative",
         name_fr: "Oeuvre Vidéo Générative",
-        name_en: ""
+        name_en: "Generative Video Work"
     }, {
-        id: 84,
+        id: 94,
         name: "Estampes numériques, sculptures et vidéos",
         name_fr: "Estampes numériques, sculptures et vidéos",
-        name_en: ""
-    }, {id: 85, name: "Exposition", name_fr: "Exposition", name_en: ""}]
+        name_en: "Digital prints, sculptures and videos"
+    }, {id: 95, name: "Exposition", name_fr: "Exposition", name_en: "Exposition"}]
 };
--- src/app/datas/place_data.ts
+++ src/app/datas/place_data.ts
@@ -6,64 +6,64 @@ export const places: IResponseApi<IPlace> = {
     next: null,
     previous: null,
     results: [{
-        id: 120,
+        id: 133,
         plan: "E_Place_Victoria.jpg",
         name: "Place Victoria",
         name_fr: "Place Victoria",
-        name_en: ""
+        name_en: "Victoria Sqare"
     }, {
-        id: 121,
+        id: 134,
         plan: "F_Cite_Int.jpg",
         name: "Cité Internationale",
         name_fr: "Cité Internationale",
-        name_en: ""
+        name_en: "Cité Internationale"
     }, {
-        id: 122,
+        id: 135,
         plan: "B_Palais_Congres.jpg",
         name: "Palais des Congrès",
         name_fr: "Palais des Congrès",
-        name_en: ""
+        name_en: "Palais des Congrès"
     }, {
-        id: 123,
+        id: 136,
         plan: "D_CCMM.jpg",
         name: "Centre Commerce Mondial de Montréal",
         name_fr: "Centre Commerce Mondial de Montréal",
-        name_en: ""
+        name_en: "World Trade Centre Montréal"
     }, {
-        id: 124,
+        id: 137,
         plan: "A_Guy_Favreau.jpg",
         name: "Complexe Guy-Favreau",
         name_fr: "Complexe Guy-Favreau",
-        name_en: ""
-    }, {id: 125, plan: null, name: "Agora UQAM ", name_fr: "Agora UQAM ", name_en: ""}, {
-        id: 126,
+        name_en: "Guy-Favreau Complex"
+    }, {id: 138, plan: null, name: "Agora UQAM ", name_fr: "Agora UQAM ", name_en: "Agoraa UQAM"}, {
+        id: 139,
         plan: null,
         name: "Château Dufresne ",
         name_fr: "Château Dufresne ",
-        name_en: ""
+        name_en: "Château Dufresne "
     }, {
-        id: 127,
+        id: 140,
         plan: "C_Jacques_Parizeau.jpg",
-        name: "Jacques Parizeau",
-        name_fr: "Jacques Parizeau",
-        name_en: ""
+        name: "Édifice Jacques Parizeau",
+        name_fr: "Édifice Jacques Parizeau",
+        name_en: "Édifice Jacques-Parizeau"
     }, {
-        id: 128,
+        id: 141,
         plan: null,
         name: "Institut Culturel du Mexique",
         name_fr: "Institut Culturel du Mexique",
-        name_en: ""
-    }, {id: 129, plan: null, name: "Cinema Du Parc ", name_fr: "Cinema Du Parc ", name_en: ""}, {
-        id: 130,
-        plan: null,
-        name: "Centre Des Arts Actuels Skol ",
-        name_fr: "Centre Des Arts Actuels Skol ",
-        name_en: ""
+        name_en: "Cultural Institute of Mexico"
     }, {
-        id: 131,
+        id: 142,
+        plan: null,
+        name: "Cinema Du Parc ",
+        name_fr: "Cinema Du Parc ",
+        name_en: "Cinema du Parc"
+    }, {id: 143, plan: null, name: "Skol", name_fr: "Skol", name_en: "Skol"}, {
+        id: 144,
         plan: null,
         name: "Observatoire Place Ville Marie ",
         name_fr: "Observatoire Place Ville Marie ",
-        name_en: ""
-    }, {id: 132, plan: null, name: "Ellephant", name_fr: "Ellephant", name_en: ""}]
+        name_en: "Observatoire Place Ville Marie "
+    }, {id: 145, plan: null, name: "Ellephant", name_fr: "Ellephant", name_en: "Ellephant"}]
 };
